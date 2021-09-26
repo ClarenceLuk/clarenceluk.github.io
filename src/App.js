421984680c948fd456b2ccc22bb7e52ed3025c1f
epixage <cluk1@mail.ccsf.edu>
2021-09-26 00:52:01 -0700

    Added Menu.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import './Menu/Menu.js';
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
