1cdf2d7d177b5e31ec2c34bb367871814f622974
epixage <cluk1@mail.ccsf.edu>
2021-09-29 17:04:52 -0700

    Testing out drawer

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core';
+import { Drawer } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => {
+    console.log(theme)
+    return (
+    {
+        menuBox: {
+            height: theme.spacing(0),
+            width: theme.spacing(0),
+        },
+        menuText: {
+            color: theme.palette.text.secondary,
+        },
+    })})
 
 const Menu = () => {
-
-
+    const classes = useStyles();
     return (
-        <div>
-            Hello World
+        <div className={classes.menuBox}>
+            <div>
+                <Drawer open>
+                    Hello World
+                </Drawer>
+            </div>
         </div>
     )
 }
 
-export default Menu;
+export default Menu
