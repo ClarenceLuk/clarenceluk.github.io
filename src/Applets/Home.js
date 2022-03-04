3c85a2a08e6640e64db18d1ef5c0e502ac43d77e
epixage <cluk1@mail.ccsf.edu>
2022-03-04 13:02:04 -0800

    added theme styles

--- src/Applets/Home.js
+++ src/Applets/Home.js
@@ -3,8 +3,9 @@ import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => {
     return ({
-        root: {
-
+        home: {
+            background: theme.palette.background,
+            
         }
     })
 })
