b0934de80bf621e4352189fa7b6bdb45a861e43c
epixage <cluk1@mail.ccsf.edu>
2022-03-03 17:37:22 -0800

    Added makeStyle for home.js for styling

--- src/Applets/Home.js
+++ src/Applets/Home.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => {
+    return ({
+        root: {
+
+        }
+    })
+})
 
 const Home = () => {
-
     return (
         <div>
             Home!!
