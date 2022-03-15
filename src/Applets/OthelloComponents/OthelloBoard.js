ad4eb920db073eb34d91add220e1fc31c06e2d3e
epixage <cluk1@mail.ccsf.edu>
2022-03-14 23:07:31 -0700

    Instead of changing each cell, now the changes will be applied to an array, each cell is only responsible for displaying its peices or legal moves. added initial starting state.

--- src/Applets/OthelloComponents/OthelloBoard.js
+++ src/Applets/OthelloComponents/OthelloBoard.js
@@ -1,5 +1,5 @@
 import { TableRow } from '@material-ui/core';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import OthelloCell from './OthelloCell';
 import { makeStyles } from '@material-ui/core';
 
@@ -18,14 +18,30 @@ const useStyles = makeStyles((theme) => {
 const OthelloBoard = (props) => {
     const defaultSize = 8
     const [boardDimension, setBoardDimension] = useState(defaultSize)
-    const board = new Array(boardDimension).fill(new Array(boardDimension).fill(-1))
+    const [board, setBoard] = useState(() => new Array(boardDimension).fill(new Array(boardDimension).fill(-1)))
+
+    useEffect(() => {
+        const initialBoard = [...board].map(a => [...a])
+        initialBoard[Math.floor(boardDimension/2)][Math.floor(boardDimension/2)] = 1
+        initialBoard[Math.floor(boardDimension/2)-1][Math.floor(boardDimension/2)-1] = 1
+        initialBoard[Math.floor(boardDimension/2)-1][Math.floor(boardDimension/2)] = 2
+        initialBoard[Math.floor(boardDimension/2)][Math.floor(boardDimension/2)-1] = 2
+        setBoard(initialBoard)
+    }, [])
+
 
     const classes = useStyles()
 
     const cellClickHandler = (row, col) => {
-        board[row][col] = props.player;
-        console.log("what", board)
-        props.setPlayer(player => player == 1 ? 0 : 1)
+        if (board[row][col] == -1) {
+            setBoard(board => {
+                const newBoard = [...board].map(a => [...a])
+                newBoard[row][col] = props.player
+                return newBoard
+            })
+            props.setPlayer(player => player == 1 ? 2 : 1)
+        }
+        
     };
 
     return (
@@ -33,7 +49,7 @@ const OthelloBoard = (props) => {
             {board.map((row, i) => (
                 <div className={ classes.boardRow }>
                     {row.map((col, j) => (
-                        <OthelloCell onClick={() => cellClickHandler(i, j)} r={i} c={j} player={props.player} board={board}/>
+                        <OthelloCell onClick={() => cellClickHandler(i, j)} r={i} c={j} player={props.player} setPlayer={props.setPlayer} board={board}/>
                     ))}
                 </div>
                 )
--- src/Applets/OthelloComponents/OthelloCell.js
+++ src/Applets/OthelloComponents/OthelloCell.js
@@ -4,6 +4,9 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles((theme) => {
     return ({
         cell: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
             padding: '5px',
             border: '1px solid black',
             background: 'green',
@@ -22,24 +25,20 @@ const useStyles = makeStyles((theme) => {
             width: '50px',
             background: 'black',
         },
+        legalMoves: {
+            borderRadius: '25px',
+            height: '10px',
+            width: '10px',
+        },
     })
 })
 
 const OthelloCell = (props) => {
-    const [cellColor, setCellColor] = useState(-1)
     const classes = useStyles();
-    console.log(props)
-
-    const onClickEvent = () => {
-        console.log("clicked!")
-        if (cellColor == -1) {
-            setCellColor(props.player)
-        }
-    }
 
     return (
-        <div className={ classes.cell } onClick={() => onClickEvent()}>
-            <div className={ cellColor == 0 ? classes.blackPiece : cellColor == 1 ? classes.whitePiece : null }/>
+        <div className={ classes.cell } onClick={props.onClick}>
+            <div className={ props.board[props.r][props.c] == 1 ? classes.blackPiece : props.board[props.r][props.c] == 2 ? classes.whitePiece : null }/>
         </div>
     )
 }
